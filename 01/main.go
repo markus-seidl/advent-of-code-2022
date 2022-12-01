747b081ac3ea7d9c12a2fdf9d5711be020944c97
Markus Seidl <mark@markus-seidl.de>
2022-12-01 15:41:34 +0100

    Advent of Code Day 01

---
+++ 01/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const InputFile = "01/input.txt"
+
+func main() {
+	// Load file
+	inputBytes, err := os.ReadFile(InputFile)
+	check(err)
+	inputStr := string(inputBytes)
+
+	//
+	// Part 01
+	//
+	var elfCalories []int
+	var currentElfCalories int
+	var maxCalories int
+	lines := strings.Split(inputStr, "\n")
+	for _, line := range lines {
+		// println(line)
+
+		if line == "" {
+			if currentElfCalories > maxCalories {
+				maxCalories = currentElfCalories
+			}
+			elfCalories = append(elfCalories, currentElfCalories)
+			currentElfCalories = 0
+			continue
+		}
+
+		calories, err := strconv.Atoi(line)
+		check(err)
+		currentElfCalories += calories
+	}
+
+	var fatElf int
+	for elf, calories := range elfCalories {
+		if calories >= maxCalories {
+			println(elf+1, calories, "Max calories")
+			fatElf = elf + 1
+		} else {
+			//println(elf+1, calories)
+		}
+	}
+	println("Elf with the most calories", fatElf)
+
+	//
+	// Part 2
+	//
+	sort.Sort(sort.Reverse(sort.IntSlice(elfCalories)))
+	sumTopThree := 0
+	for i := 0; i < 3; i++ {
+		sumTopThree += elfCalories[i]
+	}
+	println("Sum Top Three", sumTopThree)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
